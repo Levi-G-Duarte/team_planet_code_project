76eb3ba50e9dcc08c1677cf2b4dbcd3073cd7ac8
Levi Duarte <lduart441@west-mec.org>
2024-08-15 17:17:45 -0700

    new function 8/15/24

--- final-code-project.js
+++ final-code-project.js
@@ -1,53 +1,93 @@
 // Import the prompt-sync library to handle user input
 const prompt = require("prompt-sync")()
 // Import the gravityFactors module which contains factors for different planets
-const gravityFactors = require("./utils/earthGravityFactors.js")
+const earthGravityFactors = require("./utils/earthGravityFactors.js")
+const alienGravityFactors = require("./utils/alienGravityFactors.js");
 // let userUnit;
 // Define a function to show user factors based on input type and value
-function showUserFactors(factorType, factorSystem, factorMeasurement, factorValue, factorPlanet) {
+function showUserFactors(factorType, factorSystem, factorMeasurement, factorValue, factorPlanets) {
     // Initialize an object to hold the results
     const results = {}
     // Declare a variable to hold the unit of measurement
-    let measurement
-    // let unit = userUnit;
-    // let planets;
+    let factor;
+    let system;
 
-    // Iterate over each item in the gravityFactors object
-    for (let planets in gravityFactors) {
-        // Calculate the factor multiplied by the input value and round it to two decimals
-        // results[planets] = parseFloat((gravityFactors[planets] * input).toFixed(2));
-        results[planets] = parseFloat((factorUnit * gravityFactors[planets]).toFixed(2));
+    switch (factorPlanets) {
+        case "Solar":
+            for (let planets in earthGravityFactors) {
+                // Calculate the factor multiplied by the input value and round it to two decimals
+                results[planets] = parseFloat((factorValue * earthGravityFactors[planets]).toFixed(2));
+            }
+            break;
+        case "Alien":
+            for (let planets in alienGravityFactors) {
+                // Calculate the factor multiplied by the input value and round it to two decimals
+                results[planets] = parseFloat((factorValue * alienGravityFactors[planets]).toFixed(2));
+            }
+            break;
+        default:
+            console.error("Not a Planet System, L rizz");
     }
+    // Iterate over each item in the gravityFactors object
     // Switch case to determine the measurement unit based on factor type
+    switch (factorMeasurement) {
+        case "mass":
+            measurement = "mass";
+            break;
+        case "distance":
+            measurement = "distance";
+            break;
+        default:
+            console.error("PLZ choose a measurement")
+    }
+
     switch (factorType) {
         case "jump":
-            measurement = 'cm'
+            factor = 'jump'
             // for (let units in results) {
             //     console.log(`Your weight on ${planets} is: ${results[planets]}kg`)
             // }
             break;
         case "weight":
-            measurement = "kg"
+            factor = "weight"
             // for (let units in results) {
             //     console.log(`Your weight on ${planets} is: ${results[planets]}lbs`)
             // }
             break;
         default:
-            measurement = "units"
+            console.error("Not a Factor Type"); 
     }
+
+    switch (factorSystem) {
+        case "metric":
+            system = "kg"
+            break;
+        case "imperial":
+            system = "cm"
+            break;
+        default:
+            console.error("You suck, L rizz")
+    }
+
     // Iterate over the results and log each one
     for (let planets in results) {
-        console.log(`Your ${factorType} on ${planets} is ${results[planets]}${measurement}`);
+        console.log(`Your ${factor} on ${planets} is ${results[[planets]]}${system}`);
     }
 }
 
 function userInput() {
-    console.log(`Enter your type of factor:`)
+    console.log(`Enter your type of factor: (e.g weight, jump or pushup)`)
     const factorType = prompt(">");
-    console.log(`Enter system of measurement:`)
+    console.log(`Enter system type: (e.g imperial or metric)`)
     const factorSystem = prompt(">");
+    console.log(`Enter measurement type: (e.g distance or mass)`)
+    const factorMeasurement = prompt(">")
+    console.log(`Enter value:`)
+    const factorValue = prompt(">")
+    console.log(`Enter planet system: (Solar or Alien)`)
+    const factorPlanets = prompt(">")
 
-    showUserFactors(factorType, factorUnit)
+    showUserFactors(factorType, factorSystem, factorMeasurement, factorValue, factorPlanets)
 }
 // Define a function to get user inputs for factor type and value
     // Prompt the user to enter the type of factor they want to calculate
--- utils/alienGravityFactors.js
+++ utils/alienGravityFactors.js
@@ -1,6 +1,7 @@
 const alienFactors = {
     Glarp: 1.234,
     Vulcan: 2.225,
-    Krypton: 0.567
+    Krypton: 0.567,
+    Malevolon: 1.344,
 }
 module.exports = alienFactors;
